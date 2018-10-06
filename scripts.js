86916e01268abc99231c259285d936580e6defa4
Michael Du Toit <michael.dtoit@gmail.com>
2018-10-06 20:04:01 +1300

    Pace calculation works with inputs (but code is a bit ugly :) ). Added plural spelling to units object.

--- scripts.js
+++ scripts.js
@@ -4,14 +4,6 @@ var units = {
     yard: 0.9144,
     mile: 1609.344
 }
-/*
-var timeHH = Number(document.getElementById("time-hh").value),
-    timeMM = Number(document.getElementById("time-mm").value),
-    timeSS = Number(document.getElementById("time-ss").value),
-    distance = Number(document.getElementById("dist").value),
-    paceMM = Number(document.getElementById("pace-mm").value),
-    paceSS = Number(document.getElementById("pace-ss").value);
-*/
 
 /*
 function calculatePace(time, distance){
@@ -28,16 +20,16 @@ function calculateDistance(time, pace){
 */
 
 function convertToMeters(value, unit){
-     if(unit == "kilometer" || unit == "km"){
+     if(unit == "kilometer" || unit == "km" || unit == "kilometers"){
         return (value * units.kilometer);
      }
-     if(unit == "mile" || unit == "mi"){
+     if(unit == "mile" || unit == "mi" || unit == "miles"){
          return (value * units.mile);
      }
-     if(unit == "yards" || unit == "y"){
+     if(unit == "yards" || unit == "y" || unit == "yard"){
          return (value * units.yard);
      }
-     if(unit == "meter" || unit == "m"){
+     if(unit == "meter" || unit == "m" || unit == "meters"){
          return value;
      }
      else {
@@ -52,16 +44,31 @@ function convertToSeconds(hh, mm, ss){
     return (int_ss + int_mm + int_hh);
 }
 
-function paceCalculator(hh, mm, ss, dist, unit){
-    var seconds = convertToSeconds(hh, mm, ss);
-    var distance = convertToMeters(dist, unit);
+function paceCalculator(){
+    var timeHH = Number(document.getElementById("time-hh").value);
+    var timeMM = Number(document.getElementById("time-mm").value);
+    var timeSS = Number(document.getElementById("time-ss").value);
+    var dist = Number(document.getElementById("dist").value);
+    var paceMM = document.getElementById("pace-mm");
+    var paceSS = document.getElementById("pace-ss");
+    var pace_unit = document.getElementById('pace_option');
+    var paceOption = pace_unit.options[pace_unit.selectedIndex].value.toLowerCase();
+    var dist_unit = document.getElementById('dist_option');
+    var distOption = dist_unit.options[dist_unit.selectedIndex].value.toLowerCase();
+    
+    var seconds = convertToSeconds(timeHH, timeMM, timeSS);
+    var distance = convertToMeters(Number(dist), distOption);
     var toSecondsPerMeter = seconds / distance;
-    var pace_min = Math.floor((toSecondsPerMeter * units.kilometer) / 60);
-    var pace_sec = Math.floor((toSecondsPerMeter * units.kilometer) % 60);
+    var pace_min = Math.floor((toSecondsPerMeter * units[paceOption]) / 60);
+    var pace_sec = ((toSecondsPerMeter * units[paceOption]) % 60);
     if (pace_sec < 9){
         pace_sec = "0" + pace_sec;
     }
-    return pace_min + ":" + pace_sec + "/km";
+    return (
+        paceMM.value = pace_min,
+        paceSS.value = pace_sec,
+        console.log(pace_min + ":" + pace_sec + "/" + paceOption)
+    );
 }
 
 function distanceCalculator(p_mm, p_ss, p_dist, t_hh, t_mm, t_ss){
